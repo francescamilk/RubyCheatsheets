c00359d99ed64ecab281a249da4d76b5068b6d0c
Francesca Santoriello <f.santoriello@hotmail.com>
2022-07-20 10:48:28 +0200

    syntax cs

---
+++ 05_Syntax.rb
@@ -0,0 +1,44 @@
+## (ROUND PARENTHESES) ##
+# They are used:
+#  - in a method definition to define parameters
+#  - in a method call to pass arguments
+
+## [SQUARE BRACKETS] ##
+# They are used:
+#  - to define an Array
+#  - to access an Array by index
+#  - to access an Hash by key
+
+## {CURLY BRACKETS} ##
+# They are used: 
+#  - to define an Hash
+#  - to inject active code in a String
+
+def greet(name)
+  return "Hello #{name.capitalize}!"
+end
+
+puts greet("mario")
+puts greet("luigi")
+
+###
+
+topics = ["Ruby", "OOP", "SQL", "JavaScript"]
+
+puts topics[0]
+puts topics[1]
+
+### 
+
+teachers = {
+  ruby: "Francesca",
+  OOP: "Raeesa",
+  SQL: "Judith",
+  JavaScript: "Joanna"
+}
+
+puts "#{teachers[:ruby]} teaches Ruby."
+puts "#{teachers[:OOP]} teaches OOP."
+
+puts "Who teaches SQL?"
+puts teachers[:SQL]
